3dfb69d23b45d7f04edc21bba33c5bb44f851946
thorsten.wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2019-02-14 16:19:47 +0100

    release 1.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 # Create new package with python setup.py sdist
 setup(
     name='lineenhancer',
-    version='1.0.3',
+    version='1.0.6',
 
     packages=['lineenhancer'],
     license='MIT',
